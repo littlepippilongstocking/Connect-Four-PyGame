a22c1f6efe18b3e41254acc25e31f6c3f677c7fd
Dilyana Koleva <64352725+littlepippilongstocking@users.noreply.github.com>
2022-08-17 16:03:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ import math
 import pygame
 import sys
 
+blue = (0, 0, 255)
+black = (0, 0, 0)
+
 rows = 6
 cols = 7
 even = 0
 odd = 1
 
 
+# Creates the command line board and fills it with zeros
 def create_board():
     board = numpy.zeros((rows, cols))
     return board
@@ -36,21 +40,25 @@ def get_next_row(board, col):
             return r
 
 
+# Prints the board from downwards
 def print_board(board):
     print(numpy.flip(board, 0))
 
 
+# Checks if the player is winning
 def winning(board, piece):
     # Check horizontal locations
     for c in range(cols - 3):
         for r in range(rows):
-            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
+                c + 3] == piece:
                 return True
 
     # Check vertical Locations
     for c in range(cols):
         for r in range(rows - 3):
-            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
+                c] == piece:
                 return True
 
     # Check for diagonals
@@ -67,35 +75,66 @@ def winning(board, piece):
                 return True
 
 
+def draw_board(board):
+    for c in range(cols):
+        for r in range(rows):
+            pygame.draw.rect(window, blue, (c * square_size, r * square_size + square_size, square_size, square_size))
+            pygame.draw.circle(window, black, (
+                int(c * square_size + square_size / 2), int(r * square_size + square_size + square_size / 2)), radius)
+
+
 board = create_board()
 game_over = False
 turn = 0
 
+# Initialises the game and adjusts the size of the window
+pygame.init()
+
+# Adjust the size to your screen
+square_size = 80
+
+width = cols * square_size
+# Adds a supplementary row for moving the element
+height = (rows + 1) * square_size
+size = (width, height)
+radius = int(square_size / 2 - 5)
+
+window = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
 while not game_over:
-    # Ask for player 1 input
-    if turn == 0:
-        col = int(input("Player 1, make your choice (0-6): "))
+    for event in pygame.event.get():
+        # Ensures the game exits once the X is clicked
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-        if is_valid(board, col):
-            row = get_next_row(board, col)
-            place(board, row, col, 1)
-            if winning(board, 1):
-                print("Congratulations, Player 1. You won!")
-                game_over = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            # Enquire Player 1 Input
+            if turn == 0:
+                col = int(input("Player 1, make your choice (0-6): "))
+
+                if is_valid(board, col):
+                    row = get_next_row(board, col)
+                    place(board, row, col, 1)
+                    if winning(board, 1):
+                        print("Congratulations, Player 1. You won!")
+                        game_over = True
 
 
-    # Ask for player 2 input
-    else:
-        col = int(input("Player 2, make your choice (0-6): "))
+            # Enquire Player 2 Input
+            else:
+                col = int(input("Player 2, make your choice (0-6): "))
 
-        if is_valid(board, col):
-            row = get_next_row(board, col)
-            place(board, row, col, 2)
-            if winning(board, 1):
-                print("Congratulations, Player 1. You won!")
-                game_over = True
+                if is_valid(board, col):
+                    row = get_next_row(board, col)
+                    place(board, row, col, 2)
+                    if winning(board, 1):
+                        print("Congratulations, Player 1. You won!")
+                        game_over = True
 
-    print_board(board)
+            print_board(board)
 
-    turn += 1
-    turn = turn % 2
+            turn += 1
+            turn = turn % 2
